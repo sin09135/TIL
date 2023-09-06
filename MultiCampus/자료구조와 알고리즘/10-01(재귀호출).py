b7121819024e4e073a06612724ac60c7fb8fdb03
sin09135 <skwkiix@gmail.com>
2023-09-07 00:39:49 +0900

    Add Readme.md

---
+++ MultiCampus/자료구조와 알고리즘/10-01(재귀호출).py
@@ -0,0 +1,12 @@
+## 함수
+def openBox() :
+    print('상자를 열기')
+    count -= 1
+    if (count == 0):
+        print('** 선물 넣기 **')
+        return
+    openBox()
+    print('상자를 닫기###')
+
+## 메인
+count = 10
