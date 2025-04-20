0eb86d34fdeee38034f55f9a04ad42190be60b54
sin09135 <skwkiix@gmail.com>
2025-04-20 23:35:21 +0900

    Add Pytorch

--- Python/DL/mnist.py
+++ Python/DL/mnist.py
@@ -1,21 +1,17 @@
 # mnist 데이터셋 -> 학습과정 생략, 추론과정만 구현
+
+#%%
 #%%
 import sys, os
 sys.path.append(os.pardir) # 부모 디렉토리 파일을 가져올 수 있도록 설정
 from dataset.mnist import load_mnist
 
-
-# %%
-import tensorflow as tf
-from tensorflow.keras import datasets
-
+#%% 
 # 데이터 로드
-(x_train, t_train), (x_test, t_test) = datasets.mnist.load_data()
-
-# 평탄화 작업
-# Flatten (1D로 변환) 및 정규화
-x_train = x_train.reshape(x_train.shape[0], -1).astype('float32') / 255.0
-x_test = x_test.reshape(x_test.shape[0], -1).astype('float32') / 255.0
+(x_train, t_train), (x_test, t_test) = \
+load_mnist(flatten = True, normalize= False) 
+# normalize 입력 이미지의 픽셀 값을 정규화(True : 0.0 ~ 0.1, False : 0~255)
+# Flatten : 1차원 배열로 만들지 설정
 
 # 출력
 print(x_train.shape)  # (60000, 784)
